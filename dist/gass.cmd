d768d88bc816d31e4587a8305be9c7fd276c1015
Casanova <contato.gabrielcasanova@gmail.com>
2023-04-28 15:44:21 -0300

    feat: gass.cmd

--- dist/gass.cmd
+++ dist/gass.cmd
@@ -1,18 +1,19 @@
-@echo off
+:: TODO (GASS): Git Account Switch Setup
 
-:: TODO (GASS): Git 
+@ECHO off
 SET /p value="Sure you want to remove git credentials? [Y/N] "
 SET "yes=Y"
 SET "no=N"
 
 if "%value%" equ "%no%" (
   color 06
-  ECHO INFO: The operation was canceled.
+  ECHO "INFO: The operation was canceled."
   CMD /k
 ) else if "%value%" equ "%yes%"  (
   cmdkey /delete:git:https://github.com
-  ECHO DONE: Credentials removed successfully!
+  ECHO "DONE: Credentials removed successfully!"
+  CMD /k
 ) else (
-  echo WARN: The user is retarded!
-  pause
+  ECHO "WARN: The user is retarded!"
+  PAUSE
 )
